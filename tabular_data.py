51b5141029426a5f3f38977941461ab37edc76aa
maxx110 <maxzibitlomej@gmail.com>
2023-10-14 22:11:08 +0100

    This is the code to clean the data

--- tabular_data.py
+++ tabular_data.py
@@ -1,57 +1,59 @@
 import pandas as pd
 import ast
 
-def safe_literal_eval(value):
-    try:
-        return ast.literal_eval(value)
-    except (ValueError, SyntaxError):
-        return []
+class DataCleaner:
+    def __init__(self, input_file, output_file):
+        self.input_file = input_file
+        self.output_file = output_file
 
-def normalize_description(desc):
-    # Replace non-ASCII characters, e.g., â€™ with their appropriate replacements
-    desc = desc.replace('â€™', "'")  # Replace â€™ with a standard single quote
-    return desc
+    def safe_literal_eval(self, value):
+        try:
+            return ast.literal_eval(value)
+        except (ValueError, SyntaxError):
+            return []
 
-def clean_tabular_data(raw_data):
-    # Make a copy of the raw_data to avoid SettingWithCopy warning
-    data_copy = raw_data.copy()
+    def normalize_description(self, desc):
+        desc = desc.replace('â€™', "'")  # Replace â€™ with a standard single quote
+        return desc
 
-    # Remove rows with missing ratings
-    rating_columns = [
-        'Cleanliness_rating',
-        'Accuracy_rating',
-        'Communication_rating',
-        'Location_rating',
-        'Check-in_rating',
-        'Value_rating'
-    ]
-    data_copy = data_copy.dropna(subset=rating_columns)
+    def clean(self):
+        # Load the raw data using pandas
+        raw_data = pd.read_csv(self.input_file)
 
-    # Combine and clean description strings
-    data_copy['Description'] = data_copy['Description'].fillna('')
-    data_copy['Description'] = data_copy['Description'].apply(normalize_description)
-    data_copy['Description'] = data_copy['Description'].apply(lambda desc: ' '.join([d.strip() for d in safe_literal_eval(desc) if d.strip() != '']))
-    data_copy['Description'] = data_copy['Description'].str.replace('About this space', '')
+        # Remove rows with missing ratings
+        rating_columns = [
+            'Cleanliness_rating',
+            'Accuracy_rating',
+            'Communication_rating',
+            'Location_rating',
+            'Check-in_rating',
+            'Value_rating'
+        ]
+        raw_data = raw_data.dropna(subset=rating_columns)
 
-    # Set default values of 1 for empty feature columns
-    default_columns = ["guests", "beds", "bathrooms", "bedrooms"]
-    data_copy.loc[:, default_columns] = data_copy[default_columns].fillna(1)
+        # Combine and clean description strings
+        raw_data['Description'] = raw_data['Description'].fillna('')
+        raw_data['Description'] = raw_data['Description'].apply(self.normalize_description)
+        raw_data['Description'] = raw_data['Description'].apply(
+            lambda desc: ' '.join([d.strip() for d in self.safe_literal_eval(desc) if d.strip() != '']))
+        raw_data['Description'] = raw_data['Description'].str.replace('About this space', '')
 
-    return data_copy
+        # Set default values of 1 for empty feature columns
+        default_columns = ["guests", "beds", "bathrooms", "bedrooms"]
+        raw_data.loc[:, default_columns] = raw_data[default_columns].fillna(1)
+
+        # Save the processed data as clean_tabular_data.csv in the same folder
+        raw_data.to_csv(self.output_file, index=False)
+
+        print("Data cleaning and processing complete. Processed data saved to", self.output_file)
 
 if __name__ == "__main__":
-    # Load the raw data using pandas
     input_file = 'listing.csv'
-    raw_data = pd.read_csv(input_file)
-
-    # Call clean_tabular_data on the raw data
-    processed_data = clean_tabular_data(raw_data)
-
-    # Save the processed data as clean_tabular_data.csv in the same folder
     output_file = 'clean_tabular_data.csv'
-    processed_data.to_csv(output_file, index=False)
 
-    print("Data cleaning and processing complete. Processed data saved to", output_file)
+    cleaner = DataCleaner(input_file, output_file)
+    cleaner.clean()
+
 
 
 
